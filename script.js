d047205fda06b9dcbef1c0889631ce72bf1eb37a
paullete <paulettenereah3@gmail.com>
2022-03-02 16:04:00 +0300

    initial project files

---
+++ script.js
@@ -0,0 +1,13 @@
+let dayRef= document.getElementById("akan-name");
+let DaysOfWeek = [
+ "Kwasi"
+ "Kwadwo"
+ "Kwabena"
+ "Kwaku"
+ "Yaw"
+ "Kofi"
+"Kwame"
+]
+let akanName = new Date();
+let akanName = birthDay.getDay()
+console.log(birthDay);
